773a9f1fd0ae0cf9361fc7abb7ea4f61d3beba22
JapleenKaur3 <jkaur4_be22@thapar.edu>
2025-07-19 01:52:02 +0530

    fix: force dynamic rendering to fix Vercel build

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import LandingContent from "./LandingContent";
 
 export default function LandingPage() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import LandingContent from "./(landing)/LandingContent";
 
 export default function Home() {
